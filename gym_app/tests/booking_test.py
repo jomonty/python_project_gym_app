58409765077bd0bb300695996491253280355927
jomonty <100322748+jomonty@users.noreply.github.com>
2022-12-06 22:29:07 +0000

    updated tests

--- gym_app/tests/booking_test.py
+++ gym_app/tests/booking_test.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import date, time
 from models.booking import Booking
 from models.member import Member
 from models.gym_class import GymClass
@@ -6,7 +7,7 @@ from models.gym_class import GymClass
 class BookingTest(unittest.TestCase):
     def setUp(self):
         self.test_member_1 = Member('Billy', 'Biceps', False, True)
-        self.test_gym_class_1 = GymClass('Erg', '2022-12-05', '07:00', 5, True)
+        self.test_gym_class_1 = GymClass('Erg', date(2022, 12, 5), time(7,0,0), 5, True)
         self.booking_1 = Booking(self.test_gym_class_1, self.test_member_1)
     
     # @unittest.skip('skipped')
--- gym_app/tests/gym_class_test.py
+++ gym_app/tests/gym_class_test.py
@@ -1,14 +1,15 @@
 import unittest
+from datetime import date, time
 from models.gym_class import GymClass
 
 class GymClassTest(unittest.TestCase):
     def setUp(self):
-        self.test_gym_class_1 = GymClass('Erg', '2022-12-05', '07:00', 5, True)
+        self.test_gym_class_1 = GymClass('Erg', date(2022, 12, 5), time(7,0,0), 5, True)
     
     # @unittest.skip('skipped')
     def test_gym_class(self):
         self.assertEqual('Erg', self.test_gym_class_1.name)
-        self.assertEqual('2022-12-05', self.test_gym_class_1.class_date)
-        self.assertEqual('07:00', self.test_gym_class_1.class_time)
+        self.assertEqual(date(2022, 12, 5), self.test_gym_class_1.class_date)
+        self.assertEqual(time(7,0,0), self.test_gym_class_1.class_time)
         self.assertEqual(5, self.test_gym_class_1.capacity)
         self.assertEqual(True, self.test_gym_class_1.is_active)
